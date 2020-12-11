9663a2461576a2cc01e88578257e2a27ef07cae1
Chamon562 <Chamon562@gmail.com>
2020-12-10 17:25:53 -0800

    [dbCards.js] Add export default to our mongoose.model and passed in are collection name cards and passe din cardSchema.

--- dbCards.js
+++ dbCards.js
@@ -1,7 +1,14 @@
 // this is going to outline the structure of the Cards inside the database the TinderCards
 import mongoose from 'mongoose';
 
-const cardScheme = mongoose.Schema({
+const cardSchema = mongoose.Schema({
     name: String,
     imgUrl: String,
 });
+
+// export default mongoose.model('define the collection name')  and pass in cardSchema right after
+// sequel called tables and mongoDB called collection is same thing
+// in node sequel you have collection> array of documents [documents] > 
+// then this can have a collection of its own > [documents]> and so on
+export default mongoose.model('cards', cardSchema)
+// now pretty much have our database structure built up
