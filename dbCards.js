ce014110806f5393d034553c8d5be9c855d8960c
Chamon562 <Chamon562@gmail.com>
2020-12-10 17:17:27 -0800

    [dbCards.js] Add cardScehma equal to mongoose.Schema and inside the peranthesis curly brackets made the name and imUrl and set both attributes to String.

--- dbCards.js
+++ dbCards.js
@@ -1,3 +1,7 @@
 // this is going to outline the structure of the Cards inside the database the TinderCards
 import mongoose from 'mongoose';
 
+const cardScheme = mongoose.Schema({
+    name: String,
+    imgUrl: String,
+});
